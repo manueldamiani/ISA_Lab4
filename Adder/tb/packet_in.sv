ec750177664a2044a728b608fd7e0687964bc35f
manueldamiani <manuel.damiani@studenti.polito.it>
2021-03-31 13:34:54 +0200

    Modified Adder packet_in

--- Adder/tb/packet_in.sv
+++ Adder/tb/packet_in.sv
@@ -1,7 +1,7 @@
 class packet_in extends uvm_sequence_item;
     rand longint A;
     rand longint B;
-	constraint my_range { A inside {[100:1000]}; B < 10*A;}
+	constraint my_range {A inside {[100:1000]}; A -> B < 10*A; }
 
     `uvm_object_utils_begin(packet_in)
         `uvm_field_int(A, UVM_ALL_ON|UVM_HEX)
